d2d497f62a131407340feded2b028c352c3d5c39
ishaankulshrestha <ishaan.kulshrestha@gmail.com>
2018-08-23 12:45:27 +0530

    First Commit

---
+++ com/ishaan/python/DS/LinkedList.py
@@ -0,0 +1,46 @@
+class Node:
+
+    def __init__(self,name):
+        self.name = name
+        self.next = None
+
+    def getName(self):
+        return self.name
+
+    def setNext(self,next):
+        self.next = next
+
+    def getNext(self):
+        return self.next
+
+class LinkedList:
+    def __init__(self,head=None):
+        self.head = head
+
+    def insert(self,name):
+        newNode = Node(name)
+        curr = self.head
+        if curr == None:
+            self.head = newNode
+            return self.head
+        while ( curr.next != None):
+            curr = curr.next
+        curr.next = newNode
+
+    def printLL(self):
+        curr = self.head
+        while(curr != None):
+            print(curr.name)
+            curr = curr.next
+
+
+ll = LinkedList()
+ll.insert("K")
+ll.insert("B")
+ll.insert("C")
+ll.insert("D")
+ll.insert("E")
+ll.insert("F")
+ll.insert("G")
+ll.insert("H")
+ll.printLL()
