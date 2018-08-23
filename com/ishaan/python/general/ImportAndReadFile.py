6f78056ab9d88cd9c5c2edf810394a0e38848367
ishaankulshrestha <ishaan.kulshrestha@gmail.com>
2018-08-23 14:47:31 +0530

    some operation with files: first commit

---
+++ com/ishaan/python/general/ImportAndReadFile.py
@@ -0,0 +1,12 @@
+filer = open("/home/ishaan/Desktop/checkmeout.txt",'r')
+count = 0
+for strngs in filer:
+    count += 1
+    print(str(count) + " " +strngs)
+filer.close()
+
+# filew = open("/home/ishaan/Desktop/checkmeout.txt",'a')
+# for nums in range(10):
+#     filew.write(" adding line number {:10}\n".format(nums))
+# filew.close()
+
